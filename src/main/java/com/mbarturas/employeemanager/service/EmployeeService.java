d84e7004c8919a9c5bbef8d41d77ba6f0e67c91e
Arturas <mbarturas@yahoo.com>
2021-03-14 13:55:25 +0200

    Employee Repo created. Closes #3.

---
+++ src/main/java/com/mbarturas/employeemanager/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.mbarturas.employeemanager.service;
+
+import com.mbarturas.employeemanager.model.Employee;
+import com.mbarturas.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
